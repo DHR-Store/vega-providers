1871f6d9f3e0eb0ce4297735d934a591e6530291
Dr. Doom <kalipusur1234@gmail.com>
2026-03-08 17:18:45 +0530

    Delete dist/desny/catalog.ts

--- dist/desny/catalog.ts
+++
@@ -1,6 +0,0 @@
-export const catalog = [
-  { title: "Home", filter: "home" },
-  { title: "Top 10", filter: "10" },
-  { title: "Movies", filter: "movie" },
-  { title: "TV Shows", filter: "show" },
-];
